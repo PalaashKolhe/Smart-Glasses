e9999e9d8b6c993b7706ab6272dbfc0cee58f4b9
m272xu <m272xu@uwaterloo.ca>
2020-11-18 22:03:25 -0500

    Hopefully rotated display using luma.core library

--- RaspiTest.py
+++ RaspiTest.py
@@ -5,56 +5,12 @@ from time import sleep
 import time
 import requests
 
-import Adafruit_GPIO.SPI as SPI
-import Adafruit_SSD1306
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
+# library for display
+from luma.core.interface.serial import i2c
+from luma.core.render import canvas
+from luma.oled.device import ssd1306, ssd1325, ssd1331, sh1106
 
-import subprocess
-# Raspberry Pi pin configuration:
-RST = None     # on the PiOLED this pin isnt used
-# Note the following are only used with SPI:
-DC = 23
-SPI_PORT = 0
-SPI_DEVICE = 0
-
-# Beaglebone Black pin configuration:
-# RST = 'P9_12'
-# Note the following are only used with SPI:
-# DC = 'P9_15'
-# SPI_PORT = 1
-# SPI_DEVICE = 0
-
-# 128x32 display with hardware I2C:
-disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
-
-# Initialize library.
-disp.begin()
-
-# Clear display.
-disp.clear()
-disp.display()
-
-# Create blank image for drawing.
-# Make sure to create image with mode '1' for 1-bit color.
-width = disp.width
-height = disp.height
-image = Image.new('1', (width, height))
-
-# Get drawing object to draw on image.
-draw = ImageDraw.Draw(image)
-
-# Draw a black filled box to clear the image.
-draw.rectangle((0,0,width, height), outline=0, fill=0)
-
-# Draw some shapes.
-# First define some constants to allow easy resizing of shapes.
-padding = -2
-top = padding
-bottom = height-padding
-# Move left to right keeping track of the current x position for drawing shapes.
-x = 0
+device = ssd1306(serial, rotate=1)
 
 ############## Weather
 api_address='http://api.openweathermap.org/data/2.5/weather?appid=0c42f7f6b53b244c78a418f4f181282a&q='
@@ -71,38 +27,13 @@ print(str(current_temp)+'C' )
 # Load default font.
 font = ImageFont.load_default()
 
-# Alternatively load a TTF font.  Make sure the .ttf font file is in the same directory as the python script!
-# Some other nice fonts to try: http://www.dafont.com/bitmap.php
-# font = ImageFont.truetype('Minecraftia.ttf', 8)
-num = 0
 while True:
-    # Draw a black filled box to clear the image.
-    draw.rectangle((0,0,width,height), outline=0, fill=0)
 
-    # Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
-    # cmd = "hostname -I | cut -d\' \' -f1"
-    # IP = subprocess.check_output(cmd, shell = True )
-    # cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
-    # CPU = subprocess.check_output(cmd, shell = True )
-    # cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
-    # MemUsage = subprocess.check_output(cmd, shell = True )
-    # cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
-    # Disk = subprocess.check_output(cmd, shell = True )
-
-    # Write two lines of text.
-
-    # draw.text((x, top),       "My name is Palaash ",  font=font, fill=255)
-    draw.text((x, top), formatted_data.title(), font=font, fill=255)
-    draw.text((x, top+8), str(current_temp) + 'C', font=font, fill=255)
-    # draw.text((x, top+8),     str(CPU), font=font, fill=255)
-    # draw.text((x, top+16),    str(MemUsage),  font=font, fill=255)
-    # draw.text((x, top+25),    str(Disk),  font=font, fill=255)
-
-    # Display image.
-    disp.image(image)
-    disp.display()
-    time.sleep(.1)
-
+    with canvas(device) as draw:
+        # draw.text((x, top),       "My name is Palaash ",  font=font, fill=255)
+        draw.text((0, 0), formatted_data.title(), font=font, fill=255)
+        draw.text((11, 0), str(current_temp) + 'C', font=font, fill=255)
+        draw.text((0, 9), "___________", fill = "white")
 
 
 ############### Take Image with Camera
